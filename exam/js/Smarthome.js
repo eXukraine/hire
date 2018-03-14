2c5975ec13596901e1984d77c7defdf9a46df665
Alexei Yamkovoi <yamkovoi.a@gmail.com>
2018-03-14 21:28:46 +0200

    Работате но только со статическим массивом :(

---
+++ exam/js/Smarthome.js
@@ -0,0 +1,19 @@
+class Smarthome {
+		constructor(model, type, state) {
+			this.model = model;
+			this.type = type;
+			this.state = state;
+		}
+		//Создание девайса
+
+	createDev(model, type, state,){
+		this.model = model;
+		this.type = type;
+		this.state = false;
+
+			
+	}
+};
+
+
+Smarthome.devise = []
--- exam/js/render.js
+++ exam/js/render.js
@@ -1,47 +1,4 @@
 'use strict'
-/*let devise = [];
-let id = 0;*/
-//let deviseLocal = JSON.parse(localStorage.devise)
-
-
-
-
-class Smarthome {
-		constructor(model, type) {
-			this.model = model;
-			this.type = type;
-
-		}
-		//Создание девайса
-
-	createDev(model, type, state){
-		this.model = model;
-		this.type = type;
-		this.state = false;
-
-		//devise[id] = this;
-		//++id;
-		//localStorage.devise = JSON.stringify(devise)
-			
-	}
-
-
-
-};
-
-
-Smarthome.arr = []
-
-/*let cond = new Conditioner;
-cond.createDev("Midea", "Conditioner")
-let fridge = new Fridge;
-fridge.createDev("Nord", "Fridge");
-let boiler = new Boiler;
-boiler.createDev("Ariston", "Boiler")*/
-
-var rootElem = document.getElementById('root2');
-
-
 
 
 function CreateDeviseForm(){
@@ -60,9 +17,6 @@ function CreateDeviseForm(){
 		select.innerHTML = '<option value="" disabled selected>Выбирите устройство</option><option value="Conditioner">Conditioner</option><option value="Fridge">Fridge</option><option value="Boiler">Boiler</option>'
 		select.placeholder = "Выбирите устройство";	
 
-
-		
-
 		model.placeholder ="Введите модель устройства";
 		model.type = "text";
 		model.id = "model";
@@ -80,19 +34,17 @@ function CreateDeviseForm(){
 
 
 
-	form.appendChild(select);
-	form.appendChild(model);
-	form.appendChild(type);
-	form.appendChild(submitBtn);
-	rootElem.appendChild(form)
-
-
+		form.appendChild(select);
+		form.appendChild(model);
+		form.appendChild(type);
+		form.appendChild(submitBtn);
+		rootElem.appendChild(form)
 
 }
 
 CreateDeviseForm()
 
-
+let rootElem = document.getElementById('root2');
 let submitBtnClick = document.getElementById("button")
 let inp = document.getElementById('select');
 let model = document.getElementById('model');
@@ -114,23 +66,39 @@ let clearHtml = function(){
 	type.value = "";
 }
 
+let validation = function(){
+
+	if (inp.value == "Fridge"){
+		let fridge = new Fridge;
+		fridge.createDev(model.value, type.value)
+		Smarthome.devise.push(fridge);
+	}
+	else if (inp.value == "Conditioner"){
+		let cond = new Conditioner;
+		cond.createDev(model.value, type.value)
+		Smarthome.devise.push(cond);
+	}
+	else if (inp.value == "Boiler") {
+		let boiler = new Boiler;
+		boiler.createDev(model.value, type.value)
+		Smarthome.devise.push(boiler);
+	}
+}
+
 
 
 
 
 let rend = function() {
-//let deviseLocal = JSON.parse(localStorage.devise)
-	for(let i = 0; i < Smarthome.arr.length; i++){
-		//console.log(devise[i])
+	for(let i = 0; i < Smarthome.devise.length; i++){
 		function render() {
 			let targetItem = document.createElement('div');
-				targetItem.innerHTML = ""
-				//rootElem.innerHTML = ""
+				targetItem.innerHTML = "";
 				targetItem.className = "rootDiv";
-				targetItem.style = "background-color: #FF4500"
+				targetItem.style = "background-color: #FF4500";
 
 			let temp = document.createElement('div');
-				temp.innerText = `Температура: 0`
+				temp.innerText = `Температура: 0`;
 
 			let temp2 = document.createElement('div');
 				temp2.innerText = 'Температура морозильной камеры: 0';
@@ -138,10 +106,9 @@ let rend = function() {
 			let windState = document.createElement('div');
 				windState.innerText = `Жалюзи: закрыты`;
 
-			let stateV = Smarthome.arr[i].state;
+			let stateV = Smarthome.devise[i].state;
 
-			let stateValue = document.createElement('p')
-				//stateValue.innerText = `Состояние: Выкл.`
+			let stateValue = document.createElement('p');
 
 			let stateItem = document.createElement('div');
 				stateItem.innerText = `Состояние: Выкл.`;
@@ -150,27 +117,23 @@ let rend = function() {
 			
 
 			let model = document.createElement('div');
-				model.innerText = `Model: ${Smarthome.arr[i].model}`;
+				model.innerText = `Model: ${Smarthome.devise[i].model}`;
 				model.className = "model";
 
 			let item = document.createElement('div');
-				item.innerText = `Type: ${Smarthome.arr[i].type}`;
+				item.innerText = `Type: ${Smarthome.devise[i].type}`;
 
 						//Удаление 
-debugger
+
 			let dellBtn = document.createElement('button');
-				//dellBtn.innerText = "";
 				dellBtn.className = 'dellBtn'
-				//dellBtn.addEventListener("click", function(){ this.devise.splice(devise[i],1)} )
 				dellBtn.addEventListener('click', () => targetItem.innerHTML = "")
-				//dellBtn.addEventListener('click', delDev())
 				dellBtn.addEventListener('click', () => rootElem.innerHTML = "" )
 				
 				dellBtn.addEventListener('click', () => {
-					let dellIndex = Smarthome.arr.indexOf(Smarthome.arr[i]);
-						Smarthome.arr.splice(dellIndex,1)
+					let dellIndex = Smarthome.devise.indexOf(Smarthome.devise[i]);
+						Smarthome.devise.splice(dellIndex,1)
 					})
-				//targetItem.innerHTML = ""
 				dellBtn.addEventListener('click', () => rend())
 
 
@@ -178,24 +141,24 @@ debugger
 			let onBtn = document.createElement('button');
 				onBtn.innerText = 'On';
 				onBtn.className = 'on'
-				onBtn.addEventListener('click', () => Smarthome.arr[i].on)
-				onBtn.addEventListener('click', () => {if (Smarthome.arr[i].state === true){
+				onBtn.addEventListener('click', () => Smarthome.devise[i].on)
+				onBtn.addEventListener('click', () => {if (Smarthome.devise[i].state === true){
 					stateItem.innerText = `Состояние: ${stateV?'Выкл.':'Вкл.'}`;
 					targetItem.style = "background-color: #90EE90"
 					}
 				})
 
-				onBtn.addEventListener('click', () => temp.innerText = `Температура: ${Smarthome.arr[i].temp}`)
-				onBtn.addEventListener('click', () => windState.innerText = `Жалюзи: ${Smarthome.arr[i].windState} градусов`) 
-				onBtn.addEventListener('click', () => temp2.innerText = `Температура морозильной камеры: ${Smarthome.arr[i].temp2}`)
+				onBtn.addEventListener('click', () => temp.innerText = `Температура: ${Smarthome.devise[i].temp}`)
+				onBtn.addEventListener('click', () => windState.innerText = `Жалюзи: ${Smarthome.devise[i].windState} градусов`) 
+				onBtn.addEventListener('click', () => temp2.innerText = `Температура морозильной камеры: ${Smarthome.devise[i].temp2}`)
 
 				
 
 			let offBtn = document.createElement('button');
 				offBtn.innerText = "Off";
 				offBtn.className = 'off'
-				offBtn.addEventListener('click', () => Smarthome.arr[i].off)
-				offBtn.addEventListener('click', () => {if (Smarthome.arr[i].state === false){
+				offBtn.addEventListener('click', () => Smarthome.devise[i].off)
+				offBtn.addEventListener('click', () => {if (Smarthome.devise[i].state === false){
 					stateItem.innerText = `Состояние: Выкл.`;
 					targetItem.style = "background-color: #FF4500"
 					}
@@ -206,37 +169,34 @@ debugger
 
 			let windUpBtn = document.createElement('button');
 				windUpBtn.innerText = 'Up';
-				windUpBtn.addEventListener('click', () => Smarthome.arr[i].windUp());
-				windUpBtn.addEventListener('click', () => { windState.innerText = `Жалюзи: ${Smarthome.arr[i].windState} градусов`})
+				windUpBtn.addEventListener('click', () => Smarthome.devise[i].windUp());
+				windUpBtn.addEventListener('click', () => { windState.innerText = `Жалюзи: ${Smarthome.devise[i].windState} градусов`})
 
 			let windDwBtn = document.createElement('button');
 				windDwBtn.innerText = 'Dwn'
-				windDwBtn.addEventListener('click', () => Smarthome.arr[i].windDw())
-				windDwBtn.addEventListener('click', () => { windState.innerText = `Жалюзи: ${Smarthome.arr[i].windState} градусов`})
+				windDwBtn.addEventListener('click', () => Smarthome.devise[i].windDw())
+				windDwBtn.addEventListener('click', () => { windState.innerText = `Жалюзи: ${Smarthome.devise[i].windState} градусов`})
 
 
 			let tempUp = document.createElement('button');
 				tempUp.innerText = '+';
-				tempUp.addEventListener('click', () => Smarthome.arr[i].increseTemp())
-				tempUp.addEventListener('click', () => temp.innerText = `Температура: ${Smarthome.arr[i].temp}`)
+				tempUp.addEventListener('click', () => Smarthome.devise[i].increseTemp())
+				tempUp.addEventListener('click', () => temp.innerText = `Температура: ${Smarthome.devise[i].temp}`)
 
 			let tempDwn = document.createElement('button');
 				tempDwn.innerText = '-';
-				tempDwn.addEventListener('click', () => Smarthome.arr[i].decreseTemp())
-				tempDwn.addEventListener('click', () => temp.innerText = `Температура: ${Smarthome.arr[i].temp}`)
-
-			/*let temp2 = document.createElement(button)
-				temp2.innerText = "+"*/
+				tempDwn.addEventListener('click', () => Smarthome.devise[i].decreseTemp())
+				tempDwn.addEventListener('click', () => temp.innerText = `Температура: ${Smarthome.devise[i].temp}`)
 
 			let tempUp2 = document.createElement('button');
 				tempUp2.innerText = '+';
-				tempUp2.addEventListener('click', () => Smarthome.arr[i].increseTemp2())
-				tempUp2.addEventListener('click', () => temp2.innerText = `Температура морозильной камеры: ${Smarthome.arr[i].temp2}`)
+				tempUp2.addEventListener('click', () => Smarthome.devise[i].increseTemp2())
+				tempUp2.addEventListener('click', () => temp2.innerText = `Температура морозильной камеры: ${Smarthome.devise[i].temp2}`)
 
 			let temp2Dwn = document.createElement('button');
 				temp2Dwn.innerText = '-';
-				temp2Dwn.addEventListener('click', () => Smarthome.arr[i].decreseTemp2())
-				temp2Dwn.addEventListener('click', () => temp2.innerText = `Температура морозильной камеры: ${Smarthome.arr[i].temp2}`)
+				temp2Dwn.addEventListener('click', () => Smarthome.devise[i].decreseTemp2())
+				temp2Dwn.addEventListener('click', () => temp2.innerText = `Температура морозильной камеры: ${Smarthome.devise[i].temp2}`)
 
 
 				targetItem.appendChild(item);
@@ -245,7 +205,7 @@ debugger
 				stateItem.appendChild(stateValue)
 				targetItem.appendChild(onBtn);
 				targetItem.appendChild(offBtn);
-					if (Smarthome.arr[i].type == "Conditioner"){
+					if (Smarthome.devise[i].type == "Conditioner"){
 						targetItem.appendChild(windState);
 						targetItem.appendChild(windUpBtn);
 						targetItem.appendChild(windDwBtn);
@@ -254,7 +214,7 @@ debugger
 				targetItem.appendChild(tempUp);
 				targetItem.appendChild(tempDwn);
 
-					if(Smarthome.arr[i].type === "Fridge"){
+					if(Smarthome.devise[i].type === "Fridge"){
 						targetItem.appendChild(temp2)
 						targetItem.appendChild(tempUp2)
 						targetItem.appendChild(temp2Dwn)
@@ -272,27 +232,7 @@ debugger
 
 
 
- let validation = function(){
 
-	if (inp.value == "Fridge"){
-		let fridge = new Fridge;
-		fridge.createDev(model.value, type.value)
-		Smarthome.arr.push(fridge);
-		//console.log(fridge)
-	}
-	else if (inp.value == "Conditioner"){
-		let cond = new Conditioner;
-		cond.createDev(model.value, type.value)
-		Smarthome.arr.push(cond);
-		//console.log(cond)
-	}
-	else if (inp.value == "Boiler") {
-		let boiler = new Boiler;
-		boiler.createDev(model.value, type.value)
-		Smarthome.arr.push(boiler);
-		//console.log(boiler)
-	}
-}
 
 
 
